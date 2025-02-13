d0c4e48df5386c10716f4342d41b9d8762f32d98
singleparadox <glennmatthew_perez@hotmail.com>
2025-02-13 16:08:32 +0800

    chore: test

---
+++ astro.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'astro/config';
+
+import expressiveCode from 'astro-expressive-code';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import spectre from './package/src';
+
+import node from '@astrojs/node';
+import { spectreDark } from './src/ec-theme';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://singleparadox.github.io',
+  base: '/',
+  output: 'static',
+  integrations: [
+    expressiveCode({
+      themes: [spectreDark],
+    }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: 'Glenn Perez',
+      openGraph: {
+        home: {
+          title: 'Glenn Perez',
+          description: 'A minimalistic theme for Astro.'
+        },
+        blog: {
+          title: 'Blog',
+          description: 'A place where I put my experiences and thoughts.'
+        }
+        // projects: {
+        //   title: 'Projects'
+        // }
+      },
+      giscus: {
+        repository: 'louisescher/spectre',
+        repositoryId: 'R_kgDONjm3ig',
+        category: 'General',
+        categoryId: 'DIC_kwDONjm3is4ClmBF',
+        mapping: 'pathname',
+        strict: true,
+        reactionsEnabled: true,
+        emitMetadata: false,
+        lang: 'en',
+      }
+    })
+  ],
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
+});
---
+++ src/pages/styles/giscus.ts
@@ -0,0 +1,14 @@
+// export const prerender = false;
+export const prerender = true;
+
+import css from '../../styles/giscus.css?raw';
+
+export async function ALL() {
+  const res = new Response(css);
+
+  res.headers.set('Content-Type', 'text/css');
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+
+  return res;
+}
